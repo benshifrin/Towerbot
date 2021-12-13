dc06dfa218a161166db94619c625166b762ee874
Shayight <tiniwolf76@gmail.com>
2021-12-13 15:50:06 -0500

    Changes because I made mistakes

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import frc.robot.subsystem.*;
+
+public class RobotContainer {
+
+    public static DriveSubsystem m_drive;
+
+    public RobotContainer() {
+
+    }
+}
--- src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -1,22 +1,24 @@
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+package frc.robot.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.*;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveSubsystem extends Subsystem {
-    public TalonSRX _FR,_FL,_RL,_RR;
+    public WPI_TalonSRX _FR,_FL,_RL,_RR;
     public DifferentialDrive _drive;
 
 
     public DriveSubsystem() {
-        _FL = TalonSRX(6);
-        _RL = TalonSRX(1);
+        _FL = new WPI_TalonSRX(6);
+        _RL = new WPI_TalonSRX(1);
         SpeedControllerGroup leftSide = new SpeedControllerGroup(_FL, _RL);
 
         
-        _FR = TalonSRX(4);
-        _RR = TalonSRX(7);
+        _FR = new WPI_TalonSRX(4);
+        _RR = new WPI_TalonSRX(7);
         SpeedControllerGroup rightSide = new SpeedControllerGroup(_FR, _RR);
 
         _drive = new DifferentialDrive(leftSide, rightSide);
@@ -24,6 +26,10 @@ public class DriveSubsystem extends Subsystem {
 
     public void TankDrive(double leftSpeed, double rightSpeed) {
         _drive.tankDrive(leftSpeed, rightSpeed);
+    }
+
+    public void initDefaultCommand() {
+        
     }
 }
 
